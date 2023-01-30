fa0885867b7f3f8695e383d0c24d72b7f70fa4dd
kumataro689 <114778295+kumataro689@users.noreply.github.com>
2023-01-31 08:11:49 +0900

    提出用

---
+++ Ruby/menseki.rb
@@ -0,0 +1,6 @@
+print "長方形の短辺を入力して下さい\n" 
+tanpen=readline.to_f
+print "長方形の長辺を入力して下さい\n"
+tyouhen=readline.to_f
+menseki=tyouhen*tanpen
+print "長方形の面積は：",menseki,"\n"
---
+++ index.php
@@ -0,0 +1,262 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+
+    <meta charset="utf-8" />
+
+    <title>まるmaru
+    </title>
+
+    <!--https://totta.in/-->
+
+    <meta name="robots" content="noindex"> <!--検索サイトに登録されないようにする-->
+	
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+   <link rel="stylesheet" href="background.css">
+   <link rel="stylesheet" href="btn.css">
+   <link rel="stylesheet" href="background.css">
+   <script type="text/javascript" src="pass.js"></script>
+    <style>
+        table,td,th {
+            border: 1px solid #000;
+            padding: 5px;
+            text-align: center;
+        }
+    </style>
+   
+
+</head>
+
+<body background="四角模様.png"></body>  
+<link rel="stylesheet" href="background.css">
+    <!--
+       <div style="background: url(四角模様.png);">
+    <p>ここにコンテンツ</p>
+    </div>
+       背景-->
+
+  <body><font size="5">  :..:..:..:..:制作途中:..:..::...:.:..</font></body>
+  <input type="button" value="作業場所" onclick="gate();">
+  <p><img src="blue1.png" alt="写真" width="825" height="120"> </p>
+
+  
+
+<div class='youtubebtn'style="position: absolute; left: 30px; top: 80px;"/>
+    <a href='https://www.youtube.com/'target="_blank">youtube</a>
+</div>
+
+<div class='twittrbtn'style="position: absolute; left: 180px; top: 80px;"/>
+    <a href='https://www.twitter.com/'target="_blank">twitter</a>
+</div>
+<div class='twichbtn'style="position: absolute; left: 330px; top: 80px;"/>
+    <a href='https://www.twitch.tv/?lang=ja'target="_blank">twich</a>
+</div>
+<div class='amazonvideobtn'style="position: absolute; left: 480px; top: 80px;"/>
+    <a href='https://www.amazon.co.jp/gp/video/storefront/'target="_blank">amazonvideo</a>
+</div>
+<!--
+    <ul style="list-style-type:circle;">
+<li><a href='composertest/'target="_blank">amazonvideo</a></li>
+<li><a href='https://www.amazon.co.jp/gp/video/storefront/'target="_blank">amazonvideo</a></li>
+<li>豚肉</li>
+<li>にんじん</li>
+<li>たまねぎ</li>
+</ul>-->
+<div style="width:500px;">
+<div style="border: 1px dashed #333333; background-color: #2cabab;text-align:center">
+<h2>
+        <p><a href="#前置き">前置き</a></p>
+        <a href="#C:\Users>echo 1月24日">#C:\Users>echo 1月24日</a>
+        <a href="#C:\Users>echo 1月25日">#C:\Users>echo 1月25日</a>
+        <a href="#C:\Users>echo 1月26日">#C:\Users>echo 1月26日</a>
+        <a href="#C:\Users>echo 1月27日">#C:\Users>echo 1月27日</a>
+        <a href="#C:\Users>echo 1月28日">#C:\Users>echo 1月28日</a>
+        <a href="#C:\Users>echo 1月29日">#C:\Users>echo 1月29日</a>
+        <p><a href="#today">today</a></p></h2>
+        
+        
+
+        </div>
+        </div>
+
+<div style="padding: 10px; margin-bottom: 10px; border: 1px solid #333333; background-color: #FF3333;">
+<h2 id="静岡オンライン">静岡オンライン</h2>
+</div>
+<a href= "https://www.hellowork.mhlw.go.jp/kensaku/GECA110010.do?screenId=GECA110010&action=kyujinhyoBtn&kJNo=2201031759021&kJKbn=1&iNFTeikyoRiyoDtiID=&kSNo=" target="_blank" >求人票</a>
+
+
+<div style="padding: 10px; margin-bottom: 10px; border: 1px solid #333333; background-color: #f0f8ff">
+<body>
+<h1 id="前置き"><font color="#191970">前置き</font></h1>
+<h2>
+<p>現在私はこんなご時世もあり、在宅ワークを中心にして生活がなりたてられないかなと一念発起している最中だ。</p>
+<p>そんな中、1月中旬私はとある求人が気になった。それは株式会社静岡オンラインの求人だ。</p>
+<p>仕事内容はRudy,PHP,HTML,CSS,JavaScriptを中心とした作業だそうだ。RudyやPHPは触ったことがなく少々不安であったが、「プログラミングのご経験の有無は問いません」と書いてあるのでなんとかなるだろうと思い応募することにしてみた。</p>
+<p>しかし応募することをきめたがいいが一つ気になることがある。それは「業務に慣れてくれば週２回までリモート勤務ＯＫ」と書いてあることだ。</p>
+<p>在宅ワークで検索したはずだから大丈夫、、、だよな？と思いつつも1月23日思い立って電話してみることにした。</p>
+<p>『もしもし、、、？あの（中略）』どうやら担当者が不在らしい。折り返しかけてくださるようだ。ありがたい</p>
+<p>・・・ブーブーブー</p>
+<p>『はいっ！？』（余談だが急に電話なるとびっくりするよね）</p>
+<p>『こちら静岡オンラインーーー』</p>
+<p>　　　　　　　・</p>
+<p>　　　　　　　・</p>
+<p>　　　　　　　・</p>
+<p>要約するとこうだ</p>
+<p>１.完全に在宅がしたければそれなりの実力がいる</p>
+<p>２.さっさとgithubでphpとかRubyとかで作ったものを送ってこい　メールでな</p>
+<p>ということらしい（そういや担当の方喉痛めていたけれども大丈夫だったのかしら）</p>
+<p>んで何を思ってしまったのか私は</p>
+<p>『数日から一週間の間に出します』</p>
+<p>と答えてるんだなこれが</p>
+<p>いや言い訳をさせてくれ</p>
+<p>だって早い方がいいと思ったんだもん！あと電話で直接やり取りすると焦っちゃうよね！そんなことない？そんなことあるんだよ！てかなんで言った？</p>
+<p>てなわけでわざわざ自分の首を絞めてしまった一周間のLOGを下記に記載する</p>
+<p>お見苦しいものを大変申し訳ございません。なお、一部のものに多少の誇張や言葉遣いの粗さがありますがご了承お願いします。</p>
+<h2>
+<hr><!--線-->
+<h2 id="C:\Users>echo 1月24日"><font color="#191970">C:\Users>echo 1月24日</font></h2>
+<p>数日から一週間って答えてしまったなぁとりあえずgithub聞いたこともあるし知ってるんだけど肝心なことに使ったことがないんだよねぇ</p>
+<p>でも提出するためにはこれに入れて出さなきゃいけないし使ってみよう。なになにここにユーザーとパスワードを、、、パスワードが通らないんだけど！？</p>
+<p>え、（一時間後）いやなんで！？（もうしばらくして）あぁ、、、！（）あ！これパスワードってかいてあるくせにトークン入力じゃないか！</p>
+<p>いやなに食わぬ顔でpassword:とか書かないで！？お願いだから更新してよぉぉ！</p>
+<p>ちちかれた、、、そういやhtmlも業務内容的なことかいてあったな久しぶりにレンタルサーバーでも動かして、、、うーん一個ずつフォルダ動かすのめんどくさいなまとめて移動できないもんか</p>
+<p>お、FTP通信すればいけるくさいな　ん？あれうまくうごかな、、あ、うご、、動いた！　あぁもうこんな時間かおやすみなさい</p>
+<hr><!--線-->
+<h2 id="C:\Users>echo 1月25日"><font color="#191970">C:\Users>echo 1月25日</font></h2>
+<p>昨日は結局いろいろ調整して終わったなぁ</p>
+<p>そうだ！レンタルサーバーあるしページつくってそのまま送るか！そっちの方が楽だし！（今考えたけどなにが楽だったんだろう）</p>
+<p>えーとFTP通信ちゃんと動くな、よしよし。どうせだから便利そうなページつくろーっと</p>
+<p>あと友達にもなにかと便利で共有しやすいページも作っておいてと、、、（なんかいろいろ）あ、やばい脱線してたもういい時間だおやすみなさい</p>
+<hr><!--線-->
+<h2 id="C:\Users>echo 1月26日"><font color="#191970">C:\Users>echo 1月26日</font></h2>
+<p>結局昨日はページ作れずに終わってしまった</p>
+<p>静岡オンライン様に出すようにページをつくらないと、</p>
+<p>あ、でも便利だからyoutubeとかもおいていいて、（無駄）ん？ボタン配置がうまくできないてか背景がうまく反映されてないし、あれこのタグとこのタグってetc</p>
+<hr><!--線-->
+<h2 id="C:\Users>echo 1月27日"><font color="#191970">C:\Users>echo 1月27日</font></h2>
+<p>うまく配置できないなとか色とか気になったりとか結局なんのページ作るのだったのかしらとか考えていたらいつの間にか4日目そういやRubyとかまだノータッチだった。</p>
+<p>いやでも成果物ぽいのつくらないといけないしいじってるカタンのコピーサイトを成果物としてもう一度いじりなおして、、、</p>
+<p>ちょっとまてカタンのページってローカルで動けるようにしたけどローカル以外まだ動かせないよな</p>
+<p>理由がたしかローカルでWebSocket通信はうまくできるんだけどレンタルサーバーでしようとするとetc</p>
+<hr><!--線-->
+<h2 id="C:\Users>echo 1月28日"><font color="#191970">C:\Users>echo 1月28日</font></h2>
+<p>んでレンタルサーバーでWebSocketができない理由が、、、あ、PHPってこうやって使うのか</p>
+<p>おーレンタルサーバーでコンソール動かせるようになったよ感動これでphpも稼働できるな</p>
+<p>でもなんかハンドシェイクがうまくいかないうーーーん</p>
+<hr><!--線-->
+<h2 id="C:\Users>echo 1月29日"><font color="#191970">C:\Users>echo 1月29日</font></h2>
+<p>あいろいろ悩んでたら提出最終締め切り日が明日じゃないかやばいなあんかエセページつくっただけでまだなにも作ってないしカタンもうまくいかないしえ、なにを提出して形を保っておけば、、、！</p>
+<p>うーんそういえばRubyさわってないさわらないと　でもかたn(作業)</p>
+<hr><!--線-->
+<h2 id="C:\Users>echo 1月30日"><font color="#191970">C:\Users>echo 1月30日</font></h2>
+<p>なんやかんやで一週間がすぎてしまった。明日から三日間ほど家の手伝いで駆り出されるため今日中に静岡オンラインに提出しなければ</p>
+<p>というかこの段階で気づいたのだがRubyってゲームとかを作るプログラムだからまぁその辺のコードを引っ張て来て動かせればそれでいいのかなとかぼんやり考えていたのだけれど、</p>
+<p>Rubyの必要されているスキルってもしかしてウェブアプリケーションのほうでは！？Rilsでフレームワークをつくるのか。くっ気づくのが遅かった環境から作らねば</p>
+<p>あーーー！やっぱりRilsのために環境整えようとするとエラーでますよね！そうですよね！チュートリアルはチュートリアルじゃないだいたいなんかエラーがでる</p>
+<p>この感じ環境整えるだけで1日はつぶれるうっまにあわっ、（ぶっ通し）</p>
+
+<h1 id="today"><font color="#191970">today</font></h1>
+<h2>
+<p>今日です　6時です　やべぇ間に合わねぇと思ってこの記事を書き始めたのが4時でした。え、？うせやろ？</p>
+<p>まだgithubも履歴書準備してないよ！？そもそもこの記事もまだ反映させてないよ！？あと2時間後には家出るよ！？うぉぉぉ！まにあえーーー！</p>
+<p>よろしくお願いします。読みづらくて申し訳ございません。</p>
+</h2>
+</div>
+</body>
+</body>
+<?php include("file_admin/file.php"); ?>
+
+
+
+<header class="z-index_test">
+    <nav>
+        <ul>
+            <li><a href="katan">katan</a></li>
+            <li><a href="URL">メニュー2</a></li>
+            <li><a href="URL">メニュー3</a></li>
+            <li><a href="URL">メニュー4</a></li>
+        </ul>
+    </nav>
+ </header>
+
+ <body>
+
+
+   <A HREF="https://www.youtube.com/"><IMG SRC="ダウンロード.jpg"> youtube</a>
+    <div style="background-color: #85b9e9;">ボックス1</div>
+    <div style="background-color: #ffd78c;">ボックス2</div>
+    <div style="background-color: #bde9ba; position: absolute; top: 700px; left:100px;">ボックス3</div>
+
+
+
+
+
+<hr><!--線-->
+
+    <form name="myForm" method="post" action="study_database_edit.html">
+        <select id="code_selection" name="code">
+            <option value="1">テーブル作成</option>
+            <option value="2">テーブル一覧</option>
+            <option value="3">テーブル削除</option>
+        </select>
+        <p><label>テーブル名</label><input type="text" name="name" placeholder="hogehoge" pattern="^[0-9A-Za-z]+$"></p>
+        <p><input type="submit" value="実行"></p>
+    </form>
+    <?php
+        try{
+            if(isset($_POST['code'])==true){//処理コードがあるか
+                
+                //データベースにアクセス
+                $dsn = 'mysql:dbname=test;host=localhost;charset=utf8';
+                $user = 'root';
+                $password ='';
+                $dbh = new PDO($dsn,$user,$password);
+                $dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+                switch($_POST['code']){//処理コード別に処理
+                    case 1:            //データベースにテーブルを作成する処理
+                        if(isset($_POST['name'])==true){
+                            //データベースに対して実行するSQL文を作成
+                            $sql = 'CREATE TABLE IF NOT EXISTS '.$_POST['name'].' (id INT(11) NOT NULL auto_increment PRIMARY KEY,hogehoge TEXT) DEFAULT CHARSET="utf8"';
+                            //SQL文を実行
+                            $result = $dbh->query($sql);
+                        }
+                        break;
+                    case 2:            //データベースのテーブルを表示する処理
+                        print '<table><tr><th>№</th><th>テーブル名</th></tr>';
+                        if(isset($_POST['name'])==true){
+                            //データベースに対して実行するSQL文を作成
+                            $sql = 'SHOW TABLES FROM test';
+                            //SQL文を実行
+                            $table_stmt=$dbh->prepare($sql);
+                            $table_stmt->execute();
+                            //行番号用変数を用意
+                            $i=1;
+                            //データベースのテーブルすべて読み出すまでループ
+                            while($table_rec = $table_stmt->fetch(PDO::FETCH_ASSOC)){
+                                //連想配列すべてを読み出すまでループ
+                                foreach($table_rec as $key => $val){
+                                    //番号とテーブル名とキーを表示
+                                    print '<tr><td>'.$i.'</td><td>'.$val.'('.$key.')</td></tr>';
+                                    $i+=1;
+                                }
+                            }
+                        }
+                        print '</table>';
+                        break;
+                }
+            }
+        }
+        catch (Exception $e){//処理でエラーが発生した時はこちらを実行する
+            print $e->getMessage();
+        }
+    ?>
+</body>
+<hr>
+<div class="tophaikei">
+    <div class="toumei"></div>
+        <h2>確認用</h2>
+</div>
+
+
+<!--================最後につけるやーつ======================================-->
+</html>
